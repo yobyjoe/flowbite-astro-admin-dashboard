d8e711c34fab2f944d65970cb724ae50b72f168a
Joe <code@weirdrooster.com>
2024-05-15 07:10:50 +1000

    astro config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,51 @@
 import { defineConfig } from 'astro/config';
-
+import DecapCMS from 'astro-decap-cms';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 const DEV_PORT = 2121;
 
-// https://astro.build/config
 export default defineConfig({
-	site: process.env.CI
-		? 'https://themesberg.github.io'
-		: `http://localhost:${DEV_PORT}`,
-	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+  site: process.env.CI
+    ? 'https://themesberg.github.io'
+    : `http://localhost:${DEV_PORT}`,
+  base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
 
-	// output: 'server',
+  server: {
+    port: DEV_PORT,
+  },
 
-	/* Like Vercel, Netlify,… Mimicking for dev. server */
-	// trailingSlash: 'always',
-
-	server: {
-		/* Dev. server only */
-		port: DEV_PORT,
-	},
-
-	integrations: [
-		//
-		sitemap(),
-		tailwind(),
-	],
+  integrations: [
+    sitemap(),
+    tailwind(),
+    DecapCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          {
+            name: 'blog',
+            label: 'Blog',
+            folder: 'src/content/blog',
+            create: true,
+            slug: '{{slug}}',
+            fields: [
+              { label: 'Title', name: 'title', widget: 'string' },
+              { label: 'Publish Date', name: 'date', widget: 'datetime' },
+              { label: 'Body', name: 'body', widget: 'markdown' },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      external: [
+        'shiki/themes/hc_light.json', // Add the JSON file here
+      ],
+    },
+  },
 });
